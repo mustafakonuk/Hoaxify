286d2f6fe1c12a3251b2ef91b156eee1bdad474e
Mustafa KONUK <mustafakonuk@outlook.com.tr>
2020-06-07 16:07:09 +0300

    Kayıt ekranı oluşturuldu

--- frontend/src/UserSignupPage.js
+++ frontend/src/UserSignupPage.js
@@ -5,7 +5,31 @@ class UserSignupPage extends React.Component {
 
     render() {
         return (
-            <h1>Sign Up</h1>
+            <form>
+                <h1>Sign Up</h1>
+                <div>
+                    <label>Username</label>
+                    <input></input>
+                </div>
+
+                <div>
+                    <label>Display Name</label>
+                    <input></input>
+                </div>
+
+                <div>
+                    <label>Password</label>
+                    <input type="password"></input>
+                </div>
+
+                <div>
+                    <label>Password Repeat</label>
+                    <input type="password"></input>
+                </div>
+
+                <button>Sign Up</button>
+            </form>
+            
         ); 
     }
 }
